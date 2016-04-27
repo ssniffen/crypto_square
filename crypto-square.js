0f129d4504d0c2e11295cce66418db36e1bc2740
ssniffen <sniffesd@dukes.jmu.edu>
2016-04-27 10:15:42 -0400

    Update crypto-square.js

--- crypto-square.js
+++ crypto-square.js
@@ -10,8 +10,31 @@ Crypto.prototype.normalizePlaintext = function() {
 
 Crypto.prototype.size = function() {
 	// body..
-	var x = this.normalizePlaintext().length;
-	return Math.ceil(Math.sqrt(x));
+	var length = this.normalizePlaintext().length;
+	return Math.ceil(Math.sqrt(length));
+};
+
+Crypto.prototype.plaintextSegments = function() {
+	var segments = [],
+		npt = this.normalizePlaintext(),
+		size = this.size();
+	for (var i = 0; i < npt.length; i += size) {
+		segments.push(npt.substr(i, size));
+	}
+	
+	return segments;
+};
+
+Crypto.prototype.ciphertext = function() {
+	var ct = "",
+		size = this.size(),
+		segs = this.plaintextSegments();
+	for (var i = 0; i < size; i += 1) {
+		for (var j = 0; j < segs.length; j += 1) {
+			ct += segs[j].charAt(i);
+		}	
+	}	
+	return ct;
 };
 
 module.exports = Crypto;
